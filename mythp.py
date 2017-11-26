db1317c590ae73dbc5eb7ce89b6a56bd48ec5e1f
stealyglint <stealyglint@gmail.com>
2017-11-26 21:33:11 +0000

    Add web capabilities using inetd

--- mythp.py
+++ mythp.py
@@ -1,5 +1,23 @@
+#!/usr/bin/python
 import bme280
+import time
+import os
+import sys
+#while True:
+temperature,pressure,humidity = bme280.readBME280All()
+#	os.system('clear')
+temp = "Temperature: " + str(format(temperature, '.2f'))
+pres =  "Pressure: " + str(format(pressure, '.2f'))
+humid = "% Humidity: " + str(format(humidity, '.2f'))
 
-temperature,pressure,humidity = readbme280all()
-
-print(temperature)
+#print ""
+#print""
+#time.sleep(5)
+while True:
+	data = sys.stdin.readline().strip()
+	if data == "":
+		print 'HTTP/1.0 200 OK'
+		print ''
+		print '<html><head><meta http-equiv="refresh" content="10"></head><body><p>'+temp+'<br />'+pres+'<br />'+humid+'<br /></p></body></html>'
+		sys.stdout.flush()
+		break;
